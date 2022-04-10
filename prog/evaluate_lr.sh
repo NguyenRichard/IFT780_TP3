5781e803a42ebda1dc12d758f391be8935ea2a98
Administrateur <richard.nguyen@usherbrooke.ca>
2022-04-10 17:45:41 -0400

    Adding all models in bash

--- prog/evaluate_lr.sh
+++ prog/evaluate_lr.sh
@@ -2,9 +2,10 @@
 
 source env/bin/activate
 
-initial_lr=0.01
-end_lr=0.05
-inc=0.005
+model=$1
+initial_lr=$2
+end_lr=$3
+inc=$4
 
 nb_epochs=1
 
@@ -13,7 +14,14 @@ default_directory="acdc_train"
 for i in $(LC_ALL=en_US.UTF-8 seq $initial_lr $inc $end_lr)
 do
   save_directory=$default_directory"/"${i//[.]/_}
-  python train.py $save_directory --data_aug --model=FullNet --dgr=4 --dnl=4 --lr=$i --load_checkpoint --save_checkpoint --dataset='./02Heart.hdf5' --num-epochs=$nb_epochs --batch_size=2
+  if [ "$model" = "FullNet" ]; then
+    echo "Start training with learning rate $i."
+    python train.py $save_directory --data_aug --model=FullNet --dgr=4 --dnl=4 --lr=$i --load_checkpoint --save_checkpoint --dataset='./02Heart.hdf5' --num-epochs=$nb_epochs --batch_size=2
+  elif [ "$model" = "UNet" ]; then
+    echo "TODO add UNet command"
+  elif [ "$model" = "ResNet" ]; then
+    echo "TODO add ResNet command"
+  fi
 
 done
 
